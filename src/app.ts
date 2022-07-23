58d95c63a1150b84115be5522a480a9b3628c156
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-07-23 23:16:35 +0200

    eslint init airbnb

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-console.log('hello');
+const button = document.querySelector('button');
+let i = 0;
+
+button.addEventListener('click', () => {
+  i++;
+  document.querySelector('span').innerText = i.toString();
+});
