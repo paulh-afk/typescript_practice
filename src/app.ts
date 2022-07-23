aab901084aba0fb14f3d007d9b8d9b2c40de35d0
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-07-23 23:49:39 +0200

    prettier init

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,6 @@ const button = document.querySelector('button');
 let i = 0;
 
 button.addEventListener('click', () => {
-  i++;
+  i += 1;
   document.querySelector('span').innerText = i.toString();
 });
